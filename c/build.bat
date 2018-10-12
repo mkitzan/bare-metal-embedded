17ffd77e25b6df5a056aea95177e14bda355ab7b
Michael Kitzan <mkitzan45@gmail.com>
2018-10-12 08:21:21 -0700

    Update c/build.bat
    "none" means linux sys calls are not available (cause they shouldn't): https://bit.ly/2Caxyq3

--- c/build.bat
+++ c/build.bat
@@ -1,5 +1,5 @@
 @echo off
 make
-arm-eabi-ld -T stm32f0x.ld -o program.elf *.o
-arm-eabi-objcopy -O binary program.elf program.bin
+arm-none-eabi-ld -T stm32f0x.ld -o program.elf *.o
+arm-none-eabi-objcopy -O binary program.elf program.bin
 openocd -s scripts/ -f board/st_nucleo_f0.cfg -c "program program.bin reset exit 0x8000000"
