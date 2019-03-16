072a3ec18f0e229dcbd3a96f4f2faff687faa1f4
Michael Kitzan <mkitzan45@gmail.com>
2019-03-15 17:08:44 -0700

    Simulator changes

--- c/k8-assembly/k8_simulator.c
+++ c/k8-assembly/k8_simulator.c
@@ -2,8 +2,6 @@
 #include "k8_program.h"
 #include "registers.h"
 
-// change interrupt handlers to source EXTI0-1
-
 void systick_interrupt() {
     // if in RUN mode, enable next step
     STATUS |= 0xC0;
@@ -25,20 +23,33 @@ void exti01_interrupt() {
     }
 }
 
-void setup(unsigned char reg, unsigned char **store) {
+void store(unsigned char *loc, unsigned char reg) {
     if(reg = 0x00) {
-        *store = &R0;
+        R0 = *loc;
     } else if(reg = 0x04) {
-        *store = &R1;
+        R1 = *loc;
     } else if(reg = 0x08) {
-        *store = &R2;
+        R2 = *loc;
     } else {
-        *store = &PC;
+        PC = *loc;
+    }
+}
+
+void load(unsigned char reg, unsigned char *loc) {
+    if(reg = 0x00) {
+        *loc = R0;
+    } else if(reg = 0x04) {
+        *loc = R1;
+    } else if(reg = 0x08) {
+        *loc = R2;
+    } else {
+        *loc = PC;
     }
 }
 
 void main() {
     // set registers
+    RA = RB = OP = 0;
     PC = R0 = R1 = R2 = 0;
     STATUS = 0x00;
     
@@ -54,9 +65,9 @@ void main() {
     // configures EXTI to source port A pin 00-01
     SYSCFG_EXTICR1 &= 0xFFFFFF88;
     // enable interrupt triggers on rising-edge
-    EXTI_RTSR |= 0x0000000F;
+    EXTI_RTSR |= 0x00000003;
     // disable interrupt triggers on falling-edge
-    EXTI_FTSR &= 0xFFFFFFF0;
+    EXTI_FTSR &= 0xFFFFFFFC;
     // set systick counter to 1/8th second
     STK_RVR = 0x000F4240;
     // set NVIC vector 5 lowest priority
@@ -71,77 +82,84 @@ void main() {
     
     for(;;) {
         if((STATUS & 0xF0) == 0xF0) {
-            setup((TEXT[PC] & 0x0C), &reg1);
+            OP = TEXT[PC];
+            load((OP & 0x0C), &RA);
             
-            if(TEXT[PC] & 0xC0) {
-                *reg2 = (TEXT[PC] & 0x03) + 1;
+            if(OP & 0xC0) {
+                RB = (OP & 0x03) + 1;
             } else {
-                setup((TEXT[PC] & 0x03) << 2, &reg2);
-            }
+                load((OP & 0x03) << 2, &RB);
+            } 
             
-            switch(TEXT[PC] & 0xF0) {
+            switch(OP & 0xF0) {
                 case 0x00:
-                    DATA[*reg1] = *reg2;
+                    DATA[RA] = RB;
                     break;
                 case 0x10:
-                    *reg1 = DATA[*reg2];
+                    RA = DATA[RB];
                     break;
                 case 0x20:
-                    *reg1 += *reg2;
+                    RA += RB;
                     break;
                 case 0x30:
-                    *reg1 -= *reg2;
+                    RA -= RB;
                     break;
                 case 0x40:
-                    *reg1 &= *reg2;
+                    RA &= RB;
                     break;
                 case 0x50:
-                    *reg1 |= *reg2;
+                    RA |= RB;
                     break;
                 case 0x60:
-                    *reg1 != *reg1;
-                    if((TEXT[PC] & 0x03) == 0x03) {
-                        *reg2 != *reg2;
+                    RA != RA;
+                    if((OP & 0x03) == 0x03) {
+                        RB != RB;
                     }
                     break;
                 case 0x70:
-                    *reg1 = 0;
-                    if((TEXT[PC] & 0x03) == 0x03) {
-                        *reg2 = 0;
+                    RA = 0;
+                    if((OP & 0x03) == 0x03) {
+                        RB = 0;
                     }
                     break;
                 case 0x80:
-                    if(*reg1 == *reg2) {
+                    if(RA == RB) {
                         PC += 1;
                     }
                     break;
                 case 0x90:
-                    if(*reg1 != *reg2) {
+                    if(RA != RB) {
                         PC += 1;
                     }
                     break;
                 case 0xA0:
-                    if(*reg1 < *reg2) {
+                    if(RA < RB) {
                         PC += 1;
                     }
                     break;
                 case 0xB0:
-                    if(*reg1 > *reg2) {
+                    if(RA > RB) {
                         PC += 1;
                     }
                     break;
                 case 0xC0:
-                    *reg1 += *reg2;
+                    RA += RB;
                     break;
                 case 0xD0:
-                    *reg1 -= *reg2;
+                    RA -= RB;
                     break;
                 case 0xE0:
-                    *reg1 <<= *reg1;
+                    RA <<= RA;
                     break;
                 case 0xF0:
-                    *reg1 >>= *reg2;
+                    RA >>= RB;
                     break;
+            }
+            
+            store(&RA, (OP & 0x0C));
+            
+            if(!(OP & 0xC0)) {
+                store(&RB, (OP & 0x03) << 2);
             }
             
             PC += 1;
--- c/k8-assembly/k8_simulator.h
+++ c/k8-assembly/k8_simulator.h
@@ -5,7 +5,7 @@
 unsigned char PC, R0, R1, R2;
 // [clock]00 [step]00 [mode]00 [reg]00
 unsigned char STATUS;
-// control variables
-unsigned char *reg1, *reg2;
+// temp variables
+unsigned char RA, RB, OP;
 
 #endif
