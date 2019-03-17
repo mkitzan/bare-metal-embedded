4e6e4eec593d2c4e7e10fd9f07d6fa42deaeea20
Michael Kitzan <mkitzan45@gmail.com>
2019-03-17 11:44:09 -0700

    Update k8_simulator.h

--- c/k8-assembly/k8_simulator.h
+++ c/k8-assembly/k8_simulator.h
@@ -5,7 +5,7 @@
 
 // registers
 unsigned char PC, R0, R1, R2;
-// [clock]00 [step]00 [mode]00 [reg]00
+// [clock]0 [step]0 [run]00 [mode]00 [reg]00
 unsigned char STATUS;
 // temp variables
 unsigned char RA, RB, OP;
